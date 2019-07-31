b3e45ece62459688e4632ada613a2abfbb2164d3
qiang <guyan0319@gmail.com>
2019-07-31 20:56:16 +0800

    update

--- main.go
+++ main.go
@@ -9,52 +9,65 @@ import (
 )
 
 type Table struct {
-	Name map[string][]Column
+	Comment map[string]string
+	Name    map[string][]Column
 }
 type Column struct {
-	Field string
-	Type  string
+	Field   string
+	Type    string
+	Comment string
 }
 
 func main() {
 	dbName := "yuedan_user"
 	db, err := Connect("mysql", "root:123456@tcp(127.0.0.1:3306)/"+dbName+"?charset=utf8mb4&parseTime=true")
+	//Table names to be excluded
+	exclude := map[string]int{"user_authority_log": 1}
 	if err != nil {
 		fmt.Println(err)
 	}
-	TableColumn(db, dbName)
+	t := Table{}
+	t.TableColumn(db, dbName, exclude)
 
-	//fmt.Println(db, err)
+	fmt.Println(t.Comment)
 
 }
 
-func TableColumn(db *sql.DB, dbName string) {
-	rows, err := db.Query("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" + dbName + "'")
+//Extract table field information
+func (table *Table) TableColumn(db *sql.DB, dbName string, exclude map[string]int) {
+	rows, err := db.Query("SELECT t.TABLE_NAME,t.TABLE_COMMENT,c.COLUMN_NAME,c.COLUMN_TYPE,c.COLUMN_COMMENT FROM information_schema.TABLES t,INFORMATION_SCHEMA.Columns c WHERE c.TABLE_NAME=t.TABLE_NAME AND t.`TABLE_SCHEMA`='" + dbName + "'")
 	defer db.Close()
 	defer rows.Close()
+	var name, comment string
+	var column Column
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: ", err)
 		return
 	}
+	table.Comment = make(map[string]string)
+	table.Name = make(map[string][]Column)
 	for rows.Next() {
-		var name string
-		rows.Scan(&name)
-		rowsTable, err := db.Query("SHOW COLUMNS FROM " + name + "")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Fatal error: ", err)
-			return
+		rows.Scan(&name, &comment, &column.Field, &column.Type, &column.Comment)
+		if _, ok := exclude[name]; ok {
+			continue
 		}
-		for rowsTable.Next() {
-
+		if _, ok := table.Comment[name]; !ok {
+			table.Comment[name] = comment
 		}
-
+		table.Name[name] = append(table.Name[name], column)
 	}
+
 	if err = rows.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: ", err)
 		return
 	}
 	return
 }
 
+//Generate proto files in the current directory
+func (table *Table) Generate() {
+
+}
 func Connect(driverName, dsn string) (*sql.DB, error) {
 	db, err := sql.Open(driverName, dsn)
 
---
+++ template/proto.go.tpl
@@ -0,0 +1,27 @@
+syntax = "proto3";
+package {{.Models}};
+
+// The {{.Models}} service definition.
+service {{.Models}} {
+  {{range .Funcs }}
+     Material: {{.Material}} - Count:{{.Count}}
+       rpc {{.Name}}({{.RequestName}}) returns () {{{.ResponseName}}}
+  {{ end }}
+}
+
+
+message UserRequest {
+  uint64 id = 1;
+  string name = 2;
+  string email = 3;
+  string phone= 4;
+}
+
+message UserResponse {
+  uint64 id = 1;
+  bool success = 2;
+}
+message UserFilter {
+  uint64 id = 1;
+}
+
